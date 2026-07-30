17a9cdd988903c6cfff6c7601701f38fa5ed5567
MAGGZS <magdielbr1@gmail.com>
2026-07-30 19:47:48 -0300

    merda

--- ainnn.py
+++
@@ -1,4 +0,0 @@
-tomi = input(print("pau pau pau pau, quantos pau deu??" ))
-
-
-print("vc é gay pq deu", tomi ,"viadinho")
--- frontend/src/app/(auth)/layout.tsx
+++
@@ -1,7 +0,0 @@
-export default function AuthLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <div className="min-h-dvh flex items-center justify-center bg-bg-primary px-4">
-      {children}
-    </div>
-  );
-}
