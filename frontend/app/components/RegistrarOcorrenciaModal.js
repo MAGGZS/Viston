d18a59f37d1a6f1acbde1dfced8b9ae02d4bc4d0
MAGGZS <magdielbr1@gmail.com>
2026-09-16 23:11:12 -0300

    ajsute

--- frontend/app/components/RegistrarOcorrenciaModal.js
+++ frontend/app/components/RegistrarOcorrenciaModal.js
@@ -1,4 +1,4 @@
-\'use client';
+'use client';
 import { useRef, useState } from 'react';
 import { Camera, X } from 'lucide-react';
 import { Button, Modal, Select, Textarea } from '@/app/components/ui';
